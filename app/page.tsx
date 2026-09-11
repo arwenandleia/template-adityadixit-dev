5837c564e6ef30c500d4f039566dc8e846b750d1
Aditya Dixit <arwenandleia@gmail.com>
2026-09-11 11:20:41 +0700

    minor layout changes and adding link to scaffolding docs

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+
 export default async function RootHomePage() {
   return (
     <article>
       <h1>Home Page</h1>
       <p> Simple template</p>
+      <Separator className="my-4" />
+
+      <ul>
+        <li>
+          <Link href="/docs/scaffold-nextjs-project">
+            Scaffold NextJS Project
+          </Link>
+        </li>
+      </ul>
     </article>
   );
 }
